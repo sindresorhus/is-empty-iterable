ce816d594001c1b2f164dfe715f974298373d3de
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-18 17:42:49 +0000

    Require Node.js 6, add TypeScript definition (#3)

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
-module.exports = iterable => {
+
+const isEmptyIterable = iterable => {
 	for (const _ of iterable) { // eslint-disable-line no-unused-vars
 		return false;
 	}
 
 	return true;
 };
+
+module.exports = isEmptyIterable;
+module.exports.default = isEmptyIterable;
